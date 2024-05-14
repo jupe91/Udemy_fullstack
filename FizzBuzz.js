6f635a78a4a9a6e68faef6c3e3426576c1dcff40
jupe01 <juntunenreetta@gmail.com>
2024-05-14 16:19:06 +0300

    Tehtävä whosPaying

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,24 +1,16 @@
-var output = [];
-var count = 1;
-
-function fizzBuzz() {
-
-    if (count % 3 === 0 && count % 5 === 0) {
-        output.push("FizzBuzz");
-    }
-    else if (count % 3 === 0) {
-        output.push("Fizz");
-    } 
-    else if (count % 5 === 0) {
-        output.push("Buzz");
-    }
-    else {
-        output.push(count);
-    }
+function whosPaying(names) {
     
-    count++;
-  
-    console.log(output);
-}
+    /******Don't change the code above*******/
+        
+        var length = names.length;
+    
+        var randomIndex = Math.floor(Math.random() * length);
+    
+        var selectedName = names[randomIndex];
+        
+        return selectedName + " is going to buy lunch today!";
+    
 
-fizzBuzz();
+    
+    /******Don't change the code below*******/    
+    }
---
+++ whosPaying.js
@@ -0,0 +1,11 @@
+function whosPaying(names) {
+      
+    var length = names.length;
+    var randomIndex = Math.floor(Math.random() * length);
+    var selectedName = names[randomIndex];
+        
+    return selectedName + " is going to buy lunch today!";
+}
+
+var names = ["Reetta", "Pekka", "Roosa"];
+whosPaying(names);
