2af7b1e2196f26351d7737906e3108b144cc9e94
jupe01 <juntunenreetta@gmail.com>
2024-05-17 09:41:33 +0300

    Beers laskuri refaktoroitu.

--- beers.js
+++ beers.js
@@ -2,18 +2,14 @@ function beer() {
     var bottles = 99;
 
     while (bottles > 0) {
-        if (bottles > 1) {
-            console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
-            bottles--;
-            if (bottles === 1) {
-                console.log("Take one down and pass it around, " + bottles + " bottle of beer on the wall.");
-            } else {
-                console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.");
-            }
-        } else {
-            console.log(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer.");
-            bottles--;
+        console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
+        bottles--;
+        if (bottles === 0) {
             console.log("Take one down and pass it around, no more bottles of beer on the wall.");
+        } else if (bottles === 1) {
+            console.log("Take one down and pass it around, " + bottles + " bottle of beer on the wall.");
+        } else {
+            console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.");
         }
     }
 
